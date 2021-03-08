b19eb5fdca07c20fa026d30eb55b84a387032189
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-03-09 05:36:24 +0800

    Diseño de MainFrom

--- Parcial2-ap1-20180240/UI/MainForm.Designer.cs
+++ Parcial2-ap1-20180240/UI/MainForm.Designer.cs
@@ -29,20 +29,60 @@ namespace Parcial2_ap1_20180240.UI
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.RegistroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ConsultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.RegistroToolStripMenuItem,
+            this.ConsultaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // RegistroToolStripMenuItem
+            // 
+            this.RegistroToolStripMenuItem.Name = "RegistroToolStripMenuItem";
+            this.RegistroToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.RegistroToolStripMenuItem.Text = "&Reguistro";
+            // 
+            // ConsultaToolStripMenuItem
+            // 
+            this.ConsultaToolStripMenuItem.Name = "ConsultaToolStripMenuItem";
+            this.ConsultaToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.ConsultaToolStripMenuItem.Text = "&Consulta";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "Menu Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem RegistroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ConsultaToolStripMenuItem;
     }
 }
 
--- Parcial2-ap1-20180240/UI/MainForm.cs
+++ Parcial2-ap1-20180240/UI/MainForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Parcial2_ap1_20180240.UI.Consultas;
+using Parcial2_ap1_20180240.UI.Reguistro;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,7 +17,24 @@ namespace Parcial2_ap1_20180240.UI
         public MainForm()
         {
             InitializeComponent();
+            this.RegistroToolStripMenuItem.Click += new EventHandler(this.RegistroToolStripMenuItem_ItemClicked);
+            this.ConsultaToolStripMenuItem.Click += new EventHandler(this.ConsultaToolStripMenuItem_ItemClicked);
         }
+        private void RegistroToolStripMenuItem_ItemClicked(object sender, EventArgs e)
+        {
+            var proyecto = new rProyecto();
+            proyecto.MdiParent = this;
+            proyecto.Show();
+        }
+        private void ConsultaToolStripMenuItem_ItemClicked(object sender, EventArgs e)
+        {
+            var proyecto = new cProyecto();
+            proyecto.MdiParent = this;
+            proyecto.Show();
+        }
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
 
+        }
     }
 }
